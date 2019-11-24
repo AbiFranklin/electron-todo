de1c35474e7ec934da1bb73e2c4d025cd70328ff
Abigayle Franklin <Abifranklin@gmail.com>
2019-11-24 13:50:00 -0600

    Inital menu added

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -15,5 +15,88 @@ app.on('ready', () => {
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
         slashes: true
-    }))
-})
+    }));
+
+    if (process.platform === 'darwin') {
+        const template = [
+          {
+            label: 'To Do', 
+                submenu: [
+                {
+                    label: 'Add Item',
+                },
+                {
+                    label: 'Clear Item'
+                },
+                {
+                    label: 'Quit To Do',
+                    role: 'quit'
+                }
+                ],
+          },
+          {
+            label: 'Edit', submenu: [
+              { role: 'undo' },
+              { role: 'redo' },
+              { role: 'separator' },
+              { role: 'cut' },
+              { role: 'copy' },
+              { role: 'paste' },
+              { role: 'pasteandmatchstyle' },
+              { role: 'delete' },
+              { role: 'selectall' },
+            ],
+          },
+          {
+            label: 'Help', submenu: [
+              { role: 'reload' },
+              { role: 'toggleDevTools' },
+            ],
+          },
+        ];
+        template.unshift({label: '', role: 'To Do'});
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+      } else {
+        const template = [
+            {
+                label: 'File', 
+                    submenu: [
+                    {
+                        label: 'Add Item',
+                    },
+                    {
+                        label: 'Clear Item'
+                    },
+                    {
+                        label: 'Quit To Do',
+                        role: 'quit',
+                        accelerator: 'Ctrl+Shift+Q'
+                    }
+                    ],
+              },
+            {
+              label: 'Edit', submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { role: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteandmatchstyle' },
+                { role: 'delete' },
+                { role: 'selectall' },
+              ],
+            },
+            {
+              label: 'Help', submenu: [
+                { role: 'reload' },
+                { role: 'toggleDevTools' },
+              ],
+            },
+          ];
+          Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+      }
+});
+
+//Create menu template
+// Mac App menu - used for styling so shortcuts work
